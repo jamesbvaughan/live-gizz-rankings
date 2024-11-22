1ce6a401bc4bde530cb41b6c4a219f6fbe6c28c4
James Vaughan <james@jamesbvaughan.com>
2024-11-21 22:53:27 -0800

    Update song page styling

--- src/app/albums/[albumId]/song/[songId]/page.tsx
+++ src/app/albums/[albumId]/song/[songId]/page.tsx
@@ -5,6 +5,7 @@ import { getShowTitle } from "@/utils";
 import { desc, eq } from "drizzle-orm";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 type Props = { params: Promise<{ songId: string }> };
 
@@ -39,7 +40,7 @@ export default async function Song({ params }: Props) {
 
   return (
     <div className="space-y-8">
-      <h2 className="text-6xl">{song.title}</h2>
+      <h2 className="text-4xl sm:text-6xl">{song.title}</h2>
 
       <ol className="space-y-4">
         {song.performances.map((performance, index) => {
@@ -47,9 +48,9 @@ export default async function Song({ params }: Props) {
 
           return (
             <li key={performance.id} className="flex">
-              <div className="w-12 text-4xl">{index + 1}.</div>
+              <div className="w-10 shrink-0 text-4xl">{index + 1}.</div>
 
-              <div className="flex space-x-4">
+              <div className="flex shrink-0 space-x-4">
                 <div className="aspect-square w-24 bg-gray-900">
                   {performance.show.imageUrl ? (
                     <Image
@@ -61,14 +62,22 @@ export default async function Song({ params }: Props) {
                   ) : null}
                 </div>
 
-                <div className="text-4xl">
-                  {showTitle} ({Math.round(performance.eloRating)})
+                <div>
+                  <div className="text-2xl sm:text-4xl">{showTitle}</div>
+
+                  <div className="text-gray-500">
+                    ({Math.round(performance.eloRating)})
+                  </div>
                 </div>
               </div>
             </li>
           );
         })}
       </ol>
+
+      <div>
+        <Link href={`/albums/${song.albumId}`}>Back to album</Link>
+      </div>
     </div>
   );
 }
