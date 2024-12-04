a73133f0a0a5b69ec1ecd6518c0d7a0fa525cb9d
James Vaughan <james@jamesbvaughan.com>
2024-12-04 15:10:26 -0800

    Improve the votes page

--- src/app/votes/page.tsx
+++ src/app/votes/page.tsx
@@ -1,30 +1,64 @@
 import { db } from "@/drizzle/db";
-import { getPerformancePath } from "@/utils";
+import { Vote } from "@/drizzle/schema";
+import {
+  getPerformanceById,
+  getPerformancePath,
+  getShowById,
+  getShowTitle,
+  getSongById,
+} from "@/utils";
 import Link from "next/link";
 
+function PerformanceLink({ performanceId }: { performanceId: string }) {
+  const performance = getPerformanceById(performanceId)!;
+  const performancePath = getPerformancePath(performance);
+  const show = getShowById(performance.showId)!;
+  const showTitle = getShowTitle(show);
+
+  return (
+    <Link href={performancePath} className="font-bold hover:text-red">
+      {showTitle}
+    </Link>
+  );
+}
+
+function VoteListItem({ vote }: { vote: Vote }) {
+  const performance = getPerformanceById(vote.winnerId)!;
+  const song = getSongById(performance.songId)!;
+  const losingPerformanceId =
+    vote.winnerId === vote.performance1Id
+      ? vote.performance2Id
+      : vote.performance1Id;
+
+  return (
+    <li>
+      <div>
+        {song.title}: <PerformanceLink performanceId={vote.winnerId} /> beat{" "}
+        <PerformanceLink performanceId={losingPerformanceId} />
+      </div>
+
+      <div className="text-sm text-muted">
+        <time>{new Date(vote.createdAt).toLocaleString()}</time> -{" "}
+        {vote.voterId}
+      </div>
+    </li>
+  );
+}
+
 export default async function Votes() {
-  const votes = await db.query.votes.findMany({
-    with: {
-      winner: { with: { song: true, show: true } },
-    },
-  });
+  const votes = await db.query.votes.findMany();
+  votes.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
 
   return (
     <div className="space-y-4">
       <h2 className="text-4xl">All votes</h2>
 
+      <div>{votes.length} total</div>
+
       <ol className="list-disc space-y-2 pl-4">
-        {votes.map((vote) => {
-          const performancePath = getPerformancePath(vote.winner);
-          return (
-            <li key={vote.id}>
-              {vote.voterId} voted for{" "}
-              <Link href={performancePath} className="hover:text-red">
-                {vote.winner.song.title} at {vote.winner.show.location}
-              </Link>
-            </li>
-          );
-        })}
+        {votes.map((vote) => (
+          <VoteListItem key={vote.id} vote={vote} />
+        ))}
       </ol>
     </div>
   );
