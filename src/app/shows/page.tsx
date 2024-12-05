2481f1bbf7411e98f2d827174405540c37f43df6
James Vaughan <james@jamesbvaughan.com>
2024-12-04 16:37:33 -0800

    Update the shows page design

--- src/app/shows/page.tsx
+++ src/app/shows/page.tsx
@@ -1,3 +1,4 @@
+import { Show } from "@/drizzle/schema";
 import { allShows } from "@/drizzle/seeds";
 import { getShowPath, getShowTitle } from "@/utils";
 import { Metadata } from "next";
@@ -8,35 +9,59 @@ export const metadata: Metadata = {
   title: "Shows",
 };
 
+const showsByYear: Record<string, Show[]> = {};
+
+for (const show of allShows) {
+  const year = new Date(show.date).getFullYear().toString();
+  showsByYear[year] ??= [];
+  showsByYear[year].push(show);
+}
+
+const sortedShowsByYear = Object.entries(showsByYear).sort(
+  ([yearA], [yearB]) => parseInt(yearA) - parseInt(yearB),
+);
+
+for (const [_year, shows] of sortedShowsByYear) {
+  shows.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+}
+
 export default async function ShowsPage() {
   return (
     <div className="space-y-8">
       <h2 className="text-4xl">All Shows</h2>
 
-      <div className="grid grid-cols-2 gap-4">
-        {allShows.map((show) => {
-          const showPath = getShowPath(show);
-          const showTitle = getShowTitle(show);
+      {sortedShowsByYear.map(([year, shows]) => {
+        return (
+          <div key={year} className="space-y-4">
+            <h3 className="text-2xl">{year}</h3>
 
-          return (
-            <Link key={show.id} href={showPath} className="no-underline">
-              <div className="aspect-square bg-muted-3">
-                {show.imageUrl ? (
-                  <Image
-                    src={show.imageUrl}
-                    alt={showTitle}
-                    className="aspect-square w-full"
-                    width={500}
-                    height={500}
-                  />
-                ) : null}
-              </div>
+            <div className="grid grid-cols-2 gap-4">
+              {shows!.map((show) => {
+                const showPath = getShowPath(show);
+                const showTitle = getShowTitle(show);
 
-              <div className="text-lg">{showTitle}</div>
-            </Link>
-          );
-        })}
-      </div>
+                return (
+                  <Link key={show.id} href={showPath} className="no-underline">
+                    <div className="aspect-square bg-muted-3">
+                      {show.imageUrl ? (
+                        <Image
+                          src={show.imageUrl}
+                          alt={showTitle}
+                          className="aspect-square w-full"
+                          width={500}
+                          height={500}
+                        />
+                      ) : null}
+                    </div>
+
+                    <div className="text-lg">{showTitle}</div>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
