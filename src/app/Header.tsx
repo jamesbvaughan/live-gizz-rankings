6e23238d20689c68bb7cf7a8a42575fd630d0c12
James Vaughan <james@jamesbvaughan.com>
2024-12-06 15:27:55 -0800

    Update header links

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -3,27 +3,22 @@ import Link from "next/link";
 
 import lgrHandwritten from "./lgr-handwritten.png";
 
-function HeaderMenuLink({ href, title }: { href: string; title: string }) {
-  return (
-    <Link href={href} className="no-underline">
-      {title}
-    </Link>
-  );
-}
-
 function HeaderMenu() {
   return (
     <div className="flex items-center space-x-4 text-muted">
-      <HeaderMenuLink href="/songs" title="Songs" />
-      <HeaderMenuLink href="/albums" title="Albums" />
-      <HeaderMenuLink href="/shows" title="Shows" />
+      <Link href="/songs" className="no-underline">
+        Songs
+      </Link>
+      <Link href="/albums" className="no-underline">
+        Albums
+      </Link>
+      <Link href="/shows" className="no-underline">
+        Shows
+      </Link>
 
       <Link
         href="/rank"
         className="border-2 border-muted px-2 py-1 no-underline hover:border-foreground hover:text-foreground"
-        // Prefetching this page causes an error on production because the page
-        // redirects to the clerk login page.
-        prefetch={false}
       >
         Vote
       </Link>
