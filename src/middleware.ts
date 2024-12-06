2e742cc279d8a55efecdf84bbdc4c7285388be34
James Vaughan <james@jamesbvaughan.com>
2024-12-06 15:41:50 -0800

    Skip middleware on prefetch requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,18 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    {
+      // Skip Next.js internals and all static files, unless found in search params
+      source:
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+      // Allow prefetching for all pages
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
