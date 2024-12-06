5dc7a33d7df9308469671751e3f9bcd6978ca503
James Vaughan <james@jamesbvaughan.com>
2024-12-05 22:21:11 -0800

    Add a more fun 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
 
+const notFoundMessages = [
+  "Searching... but there's nothing at this URL!",
+  "Your real's not real, and neither is this URL.",
+  "There is no Planet B. There's also no webpage at this URL.",
+];
+
 export default function NotFound() {
+  const message =
+    notFoundMessages[Math.floor(Math.random() * notFoundMessages.length)];
+
   return (
     <div className="space-y-6">
-      <p>There&apos;s nothing at this URL!</p>
+      <h2 className="text-4xl">404</h2>
+
+      <p className="text-xl">{message}</p>
+
       <p>
         <Link href="/">Go back home</Link>
       </p>
