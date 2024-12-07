73902b39ea16592d4c90f384adc1e08d21b041ec
James Vaughan <james@jamesbvaughan.com>
2024-12-06 21:25:47 -0800

    Add converge thing

---
+++ src/app/votes/Converge.tsx
@@ -0,0 +1,14 @@
+export function Converge() {
+  return (
+    <>
+      <span className="peer underline decoration-muted-2 decoration-dotted underline-offset-2">
+        converge
+      </span>
+
+      <span className="pointer-events-none fixed left-0 top-0 hidden h-screen w-screen flex-col items-center justify-center space-y-8 bg-red/50 text-[15vw] font-black peer-hover:flex">
+        <span>CONVERGE</span>
+        <span>CONVERGE</span>
+      </span>
+    </>
+  );
+}
--- src/app/votes/page.tsx
+++ src/app/votes/page.tsx
@@ -11,6 +11,8 @@ import {
   getSongById,
 } from "@/utils";
 
+import { Converge } from "./Converge";
+
 export const metadata: Metadata = {
   title: "Votes",
 };
@@ -67,7 +69,7 @@ function LeftRightStats({ votes }: { votes: Vote[] }) {
       <p>
         This ratio indicates how often voters choose the left vs the right
         performance from the voting page. That order is generated randomly on
-        each page load, so this <i>should</i> converge on 1.00.
+        each page load, so this <i>should</i> <Converge /> on 1.00.
       </p>
       <p>
         If this number is far from 1.00, then that indicates a bias toward one
