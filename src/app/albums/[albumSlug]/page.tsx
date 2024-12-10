2821489378516171eb1489fc5e7514bb3a3997cc
James Vaughan <james@jamesbvaughan.com>
2024-12-09 21:21:10 -0800

    Add a "best of" section to album pages

--- src/app/albums/[albumSlug]/page.tsx
+++ src/app/albums/[albumSlug]/page.tsx
@@ -1,10 +1,21 @@
+import { desc, eq } from "drizzle-orm";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
+import { db } from "@/drizzle/db";
+import { performances, Song } from "@/drizzle/schema";
 import { allAlbums, allSongs } from "@/drizzle/seeds";
-import { getAlbumBySlug, getSongPath } from "@/utils";
+import {
+  getAlbumBySlug,
+  getPerformancePath,
+  getPerformanceTitle,
+  getShowById,
+  getShowTitle,
+  getSongPath,
+} from "@/utils";
 
 type Params = { albumSlug: string };
 type Props = { params: Promise<Params> };
@@ -29,6 +40,29 @@ export function generateStaticParams(): Params[] {
 
 export const dynamicParams = false;
 
+async function BestPerformanceForSong({ song }: { song: Song }) {
+  const performance = await db.query.performances.findFirst({
+    where: eq(performances.songId, song.id),
+    orderBy: desc(performances.eloRating),
+  });
+
+  if (!performance) {
+    return null;
+  }
+
+  const show = getShowById(performance.showId)!;
+  const showTitle = getShowTitle(show);
+  const performancePath = getPerformancePath(performance);
+
+  return (
+    <li>
+      <Link href={performancePath} className="no-underline">
+        {song.title} - {showTitle}
+      </Link>
+    </li>
+  );
+}
+
 export default async function Album({ params }: Props) {
   const { albumSlug } = await params;
   const album = getAlbumBySlug(albumSlug);
@@ -39,10 +73,10 @@ export default async function Album({ params }: Props) {
   const songs = allSongs.filter((song) => song.albumId === album.id);
 
   return (
-    <div>
+    <div className="space-y-8">
       <div className="grid grid-cols-2">
         <div className="space-y-4">
-          <h2 className="text-2xl">{album.title}</h2>
+          <h2 className="text-4xl">{album.title}</h2>
 
           <ul>
             {songs.map((song) => {
@@ -67,7 +101,21 @@ export default async function Album({ params }: Props) {
         />
       </div>
 
-      <Link href="/albums" className="no-underline">
+      <div className="space-y-4">
+        <h3 className="text-3xl">Best performances</h3>
+
+        <p>These are the top-rated performances of the songs on this album.</p>
+
+        <Suspense fallback={<p>Loading best performances...</p>}>
+          <ul>
+            {songs.map((song) => {
+              return <BestPerformanceForSong song={song} key={song.id} />;
+            })}
+          </ul>
+        </Suspense>
+      </div>
+
+      <Link href="/albums" className="inline-block no-underline">
         Back to albums
       </Link>
     </div>
