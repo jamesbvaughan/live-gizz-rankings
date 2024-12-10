d8ddbbe475b0660f9a656f9bde016e4a4b74bb1b
James Vaughan <james@jamesbvaughan.com>
2024-12-09 21:36:28 -0800

    Format

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="mt-10 space-y-10">
       <hr className="border-red" />
 
-      <div className="text-muted my-10 flex justify-between">
+      <div className="my-10 flex justify-between text-muted">
         <div>
           made by{" "}
           <a href="https://jamesbvaughan.com" className="no-underline">
