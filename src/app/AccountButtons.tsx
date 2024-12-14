80456464e20d8654c2f8964dde9a1ad7c971d95f
James Vaughan <james@jamesbvaughan.com>
2024-12-13 17:24:29 -0800

    Add link to users page

--- src/app/AccountButtons.tsx
+++ src/app/AccountButtons.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { SignInButton, SignOutButton, useAuth } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export function AccountButtons() {
-  const { isSignedIn, isLoaded } = useAuth();
+  const { isSignedIn, isLoaded, user } = useUser();
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -15,10 +16,26 @@ export function AccountButtons() {
     return <div className="cursor-wait text-muted-2">sign in</div>;
   }
 
+  const isAdmin = user?.publicMetadata.isAdmin;
+  console.log(user);
+
   return isSignedIn ? (
-    <SignOutButton>
-      <button className="hover:text-red">sign out</button>
-    </SignOutButton>
+    <div className="flex flex-col items-end space-y-2">
+      <SignOutButton>
+        <button className="hover:text-red">sign out</button>
+      </SignOutButton>
+
+      {isAdmin ? (
+        <>
+          <hr className="w-full border-muted-2" />
+
+          <div className="flex flex-col">
+            <Link href="/users">users</Link>
+            <Link href="/votes">votes</Link>
+          </div>
+        </>
+      ) : null}
+    </div>
   ) : (
     <SignInButton mode="modal">
       <button className="hover:text-red">sign in</button>
