230fec62b5290499507a4b0c02e67a7335492176
James Vaughan <james@jamesbvaughan.com>
2025-09-28 20:53:23 -0700

    Remove seed data (#236)
    Now that all data is in the database an managed directly in the app,
    there's no more need for these seed data files. They'll still be here in
    the git history if they're ever needed.

--- bin/counts.ts
+++
@@ -1,9 +0,0 @@
-import { allAlbums } from "@/drizzle/data/albums";
-import { allPerformances } from "@/drizzle/data/performances";
-import { allShows } from "@/drizzle/data/shows";
-import { allSongs } from "@/drizzle/data/songs";
-
-console.log(`Albums:       ${allAlbums.length}`);
-console.log(`Songs:        ${allSongs.length}`);
-console.log(`Shows:        ${allShows.length}`);
-console.log(`Performances: ${allPerformances.length}`);
